607c3b1b62c29bde3f08e8e36c5a0f61a6410225
quang <quanglatoi002@gmail.com>
2022-12-22 18:01:55 +0700

    Setups react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+import { Home, Login, Public } from "./containers/public";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-    const { ...test } = useSelector((state) => state.app);
-    console.log(test);
-
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+    return (
+        <>
+            <div className="">
+                <Routes>
+                    <Route path="/*" element={<Public />}></Route>
+                </Routes>
+            </div>
+        </>
+    );
 }
 
 export default App;
