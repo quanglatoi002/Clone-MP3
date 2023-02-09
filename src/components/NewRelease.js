a179b06192856a7f4065074a108c9e9d0ff75e2d
quang <quanglatoi002@gmail.com>
2023-02-09 14:30:26 +0700

    upload new release home

--- src/components/NewRelease.js
+++ src/components/NewRelease.js
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+///
+import { Songs } from "./";
 
 const NewRelease = () => {
+    const [songs, setSongs] = useState([]);
     const { newRelease } = useSelector((state) => state.app);
     const [isActive, setIsActive] = useState(0);
 
+    useEffect(() => {
+        isActive
+            ? setSongs(newRelease?.items.others)
+            : setSongs(newRelease?.items?.vPop);
+    }, [isActive, newRelease]);
     return (
         <div className="mt-12 px-[59px] flex flex-col gap-5">
             <div className="flex items-center justify-between">
@@ -15,21 +23,32 @@ const NewRelease = () => {
             </div>
             <div className="flex items-center gap-5 text-xs">
                 <button
+                    onClick={() => setIsActive(0)}
                     type="button"
                     className={`py-1 px-4 rounded-l-full rounded-r-full border border-gray-400  ${
-                        !isActive ? "bg-main-500 text-white" : "bg-transparent"
+                        isActive === 0
+                            ? "bg-main-500 text-white"
+                            : "bg-transparent"
                     }`}
                 >
-                    VIETNAM
+                    VIỆT NAM
                 </button>
                 <button
+                    onClick={() => setIsActive(1)}
                     type="button"
                     className={`py-1 px-4 rounded-l-full rounded-r-full border border-gray-400 ${
-                        !isActive ? "bg-main-500 text-white" : "bg-transparent"
+                        isActive === 1
+                            ? "bg-main-500 text-white"
+                            : "bg-transparent"
                     }`}
                 >
-                    VIETNAM
+                    QUỐC TẾ
                 </button>
+            </div>
+            <div className="flex flex-wrap w-full ">
+                {songs.map((item) => (
+                    <Songs key={item.encodeId} data={item} />
+                ))}
             </div>
         </div>
     );
--- src/components/Songs.js
+++ src/components/Songs.js
@@ -1,7 +1,31 @@
 import { memo } from "react";
-
-const Songs = () => {
-    return <div>Songs</div>;
+import moment from "moment";
+import "moment/locale/vi";
+///
+import SongItem from "./SongItem";
+const Songs = ({ data }) => {
+    return (
+        <div className="flex flex-auto p-[10px] w-[45%] lg:w-[30%] gap-[10px] hover:bg-main-100">
+            <img
+                src={data.thumbnail}
+                alt="thumbnail"
+                className="w-[60px] h-[60px] object-cover rounded-md"
+            />
+            <div className="flex flex-col gap-1 whitespace-nowrap">
+                <span className="font-medium text-[14px] leading-[18.2px] text-primary ">
+                    {data.title.length > 30
+                        ? `${data.title.slice(0, 30)}...`
+                        : data.title}
+                </span>
+                <span className="text-xs text-secondary hover:text-hover_secondary">
+                    {data.artistsNames}
+                </span>
+                <span className=" text-xs text-secondary">
+                    {moment(data.releaseDate * 1000).fromNow()}
+                </span>
+            </div>
+        </div>
+    );
 };
 
 export default memo(Songs);
